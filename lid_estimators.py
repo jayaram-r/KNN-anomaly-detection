16591727106da45c4b7966ef5d5670753e077ae0
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2019-11-22 02:34:20 -0600

    Add estimators for ID and LID

--- lid_estimators.py
+++ lid_estimators.py
@@ -4,6 +4,7 @@ Methods for (local) intrinsic dimension estimation based on nearest neighbor dis
 """
 from __future__ import division
 import numpy as np
+from scipy import stats
 import sys
 
 
@@ -25,6 +26,37 @@ def lid_mle_amsaleg(knn_distances):
     # Replace 0 distances with a very small float value
     knn_distances = np.clip(knn_distances, sys.float_info.min, None)
     log_dist_ratio = np.log(knn_distances / knn_distances[:, -1].reshape((n, 1)))
-    lid_est = -1.0 / np.mean(log_dist_ratio, axis=1)
+    # lid_est = -k / np.sum(log_dist_ratio, axis=1)
+    lid_est = -(k - 1) / np.sum(log_dist_ratio, axis=1)
 
     return lid_est
+
+
+def id_two_nearest_neighbors(knn_distances):
+    """
+    Estimate the intrinsic dimension of the data using the Two-nearest-neighbor method proposed in the following
+    paper:
+    Facco, Elena, et al. "Estimating the intrinsic dimension of datasets by a minimal neighborhood information."
+    Scientific reports 7.1 (2017): 12140.
+
+    :param knn_distances: numpy array of k nearest neighbor distances. Has shape `(n, k)` where `n` is the
+                          number of points and `k` is the number of neighbors.
+    :return: float value estimate of the intrinsic dimension.
+    """
+    n, k = knn_distances.shape
+    # Ratio of 2nd to 1st nearest neighbor distances
+    nn_ratio = knn_distances[:, 1] / np.clip(knn_distances[:, 0], sys.float_info.min, None)
+
+    # Empirical CDF of `nn_ratio`
+    nn_ratio_sorted = np.sort(nn_ratio)
+    # Insert value 1 as the minimum value, which will have an empirical CDF of 0.
+    # This will ensure that the line passes through the origin
+    nn_ratio_sorted = np.insert(nn_ratio_sorted, 0, 1.0)
+    ecdf = np.arange(n + 1) / float(n + 1)
+
+    xs = np.log(nn_ratio_sorted)
+    ys = -1 * np.log(1 - ecdf)
+    # Fit a straight line. The slope of this line gives an estimate of the intrinsic dimension
+    slope, intercept, _, _, _ = stats.linregress(xs, ys)
+
+    return slope
