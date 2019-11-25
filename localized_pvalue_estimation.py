41642a1a0f525c1bab4bb768650fb903e4191506
Jayaram Raghuram <jayaram@Jayarams-MacBook-Pro.local>
2019-11-25 11:05:20 -0600

    Updates to support shared nearest neighbor distance measure

--- localized_pvalue_estimation.py
+++ localized_pvalue_estimation.py
@@ -64,6 +64,7 @@ class averaged_KLPE_anomaly_detection:
         self.n_jobs = n_jobs
         self.seed_rng = seed_rng
 
+        self.num_samples = None
         self.neighborhood_range = None
         self.index_knn = None
         self.dist_stat_nominal = None
@@ -76,6 +77,7 @@ class averaged_KLPE_anomaly_detection:
         :return: None
         """
         N, d = data.shape
+        self.num_samples = N
         if self.n_neighbors is None:
             # Set number of nearest neighbors based on the data size and the neighborhood constant
             self.n_neighbors = int(np.ceil(N ** self.neighborhood_constant))
@@ -85,8 +87,8 @@ class averaged_KLPE_anomaly_detection:
         high = self.n_neighbors + int(np.floor(0.5 * self.n_neighbors))
         self.neighborhood_range = (low, high)
 
-        # Build the KNN graph
-        self.index_knn = self.build_knn_graph(data)
+        # Build the KNN graphs
+        self.index_knn = self.build_knn_graphs(data)
 
         # Compute the distance statistic for every data point
         self.dist_stat_nominal = self.distance_statistic(data, exclude_self=True)
@@ -108,35 +110,77 @@ class averaged_KLPE_anomaly_detection:
         # Negative log of the p-value is returned as the anomaly score
         return -1.0 * np.log(np.clip(score, sys.float_info.min, None))
 
-    def build_knn_graph(self, data):
+    def build_knn_graphs(self, data, min_n_neighbors=20, rho=0.5):
         """
         Build a KNN index for the given data set.
         :param data: numpy data array of shape `(N, d)`, where `N` is the number of samples and `d` is the number
                      of dimensions (features).
-        :return: KNN index.
+        :param min_n_neighbors: minimum number of nearest neighbors to use for the `NN-descent` method.
+        :param rho: `rho` parameter used by the `NN-descent` method.
+        :return: A list with one or two KNN indices.
         """
+        N, d = data.shape
         if self.approx_nearest_neighbors:
             # Construct an approximate nearest neighbor (ANN) index to query nearest neighbors
             params = {
                 'metric': self.metric,
                 'metric_kwds': self.metric_kwargs,
-                'n_neighbors': max(1 + self.neighborhood_range[1], 20),
-                'rho': 0.5,
+                'n_neighbors': max(1 + self.neighborhood_range[1], min_n_neighbors),
+                'rho': rho,
                 'random_state': self.seed_rng,
                 'n_jobs': self.n_jobs,
                 'verbose': False
             }
-            index_knn = NNDescent(data, **params)
+            index_knn_primary = NNDescent(data, **params)
         else:
             # Construct the exact KNN graph
-            index_knn = NearestNeighbors(
+            index_knn_primary = NearestNeighbors(
                 n_neighbors=(1 + self.neighborhood_range[1]),
                 algorithm='brute',
                 metric=self.metric,
                 metric_params=self.metric_kwargs,
                 n_jobs=self.n_jobs
             )
-            index_knn.fit(data)
+            index_knn_primary.fit(data)
+
+        if self.shared_nearest_neighbors:
+            # Query the `self.n_neighbors` nearest neighbors of each point.
+            # Since each point will be selected as its own nearest neighbor, we query for `self.n_neighbors + 1`
+            # neighbors and ignore the self neighbors
+            nn_indices, _ = self.query_wrapper(data, index_knn_primary, self.n_neighbors + 1)
+
+            # Create the neighborhood membership vector for each point.
+            # `data_neighbors` will be numpy array of 0s and 1s, with shape `(N, N)` and dtype `np.uint8`
+            data_neighbors = neighborhood_membership_vectors(nn_indices, N)
+
+            # Set the diagonal elements of `data_neighbors` to 0 because we don't want a point to be in its
+            # own neighborhood set
+            np.fill_diagonal(data_neighbors, 0)
+
+            # Construct another KNN index for the binary membership data using the shared nearest neighbor
+            # distance metric
+            if self.approx_nearest_neighbors:
+                params = {
+                    'metric': distance_SNN,
+                    'n_neighbors': max(1 + self.neighborhood_range[1], min_n_neighbors),
+                    'rho': rho,
+                    'random_state': self.seed_rng,
+                    'n_jobs': self.n_jobs,
+                    'verbose': False
+                }
+                index_knn_secondary = NNDescent(data_neighbors, **params)
+            else:
+                index_knn_secondary = NearestNeighbors(
+                    n_neighbors=(1 + self.neighborhood_range[1]),
+                    algorithm='brute',
+                    metric=distance_SNN,
+                    n_jobs=self.n_jobs
+                )
+                index_knn_secondary.fit(data_neighbors)
+
+            index_knn = [index_knn_primary, index_knn_secondary]
+        else:
+            index_knn = [index_knn_primary]
 
         return index_knn
 
@@ -153,13 +197,43 @@ class averaged_KLPE_anomaly_detection:
             # Query an extra neighbor because the points are part of the KNN graph
             k1 = self.neighborhood_range[0] + 1
             k2 = self.neighborhood_range[1] + 1
+            k = self.n_neighbors + 1
         else:
             k1, k2 = self.neighborhood_range
+            k = self.n_neighbors
 
-        if self.approx_nearest_neighbors:
-            nn_indices, nn_distances = self.index_knn.query(data, k=k2)
+        if self.shared_nearest_neighbors:
+            # Query the `k` nearest neighbors
+            nn_indices_, _ = self.query_wrapper(data, self.index_knn[0], k)
+
+            # Create the neighborhood membership vector for each point
+            data_neighbors = neighborhood_membership_vectors(nn_indices_, self.num_samples)
+
+            if exclude_self:
+                # Set the diagonal elements of `data_neighbors` to 0 because we don't want a point to be in its
+                # own neighborhood set
+                np.fill_diagonal(data_neighbors, 0)
+
+            nn_indices, nn_distances = self.query_wrapper(data_neighbors, self.index_knn[1], k2)
         else:
-            nn_distances, nn_indices = self.index_knn.kneighbors(data, n_neighbors=k2)
+            nn_indices, nn_distances = self.query_wrapper(data, self.index_knn[0], k2)
 
         dist_stat = np.mean(nn_distances[:, (k1 - 1):], axis=1)
         return dist_stat
+
+    def query_wrapper(self, data, index, k):
+        """
+        Unified wrapper for querying both the approximate and the exact KNN index.
+
+        :param data: numpy data array of shape `(N, d)`, where `N` is the number of samples and `d` is the number
+                     of dimensions (features).
+        :param index: KNN index.
+        :param k: number of nearest neighbors to query.
+        :return:
+        """
+        if self.approx_nearest_neighbors:
+            nn_indices, nn_distances = index.query(data, k=k)
+        else:
+            nn_distances, nn_indices = index.kneighbors(data, n_neighbors=k)
+
+        return nn_indices, nn_distances
